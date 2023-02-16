758b15dacd20d613a693890aa6ddc34dc588db22
myk6 <myk6@fresnou.org>
2023-02-16 14:15:09 -0800

    initial commit

---
+++ Input2.js
@@ -0,0 +1,7 @@
+const readLine = require('readLine-sync');
+
+
+//1
+let num1 = Number(readLine.question("ENTER #:"));
+let num2 = Number(readLine.question("ENTER #:"));
+console.log(`$(numb) + $(numb2) = $(numb + numb2)`);
