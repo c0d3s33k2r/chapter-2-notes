8b215a8290b5448d87e586e56ea9c081b6038e5a
myk6 <myk6@fresnou.org>
2023-02-17 14:16:17 -0800

    I updated whileloops

---
+++ whileloops1.js
@@ -0,0 +1,41 @@
+const readLine = require('readline-sync');
+
+//1
+let A = 0;
+while (A <= 25) {
+    if (A > 0) {
+        console.log(A);
+        }
+
+    A++;
+}
+
+//2
+let B = 0;
+while (B <= 25) {
+    if (B > 0) {
+        console.log(B);
+    }
+
+    B++;
+}
+
+//3
+let C = -10;
+while (C <= 10) {
+    if (C >= -10) {
+        console.log(C);
+    }
+
+    C++;
+}
+
+//4
+let D = 0;
+while (D > -51) {
+    if (D <= 0) {
+        console.log(D);
+    }
+
+    D--;
+}
